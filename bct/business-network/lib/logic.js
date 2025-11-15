2916431875615a05f7ed4b8392ab5d75f1af1bee
Sameet Patil <sameetpatil5@gmail.com>
2025-11-15 08:31:47 +0530

    Remove code

--- bct/business-network/lib/logic.js
+++
@@ -1,8 +0,0 @@
-/**
- * Transfer an asset to another participant
- * @param {org.example.business.TransferProduct} tx
- * @transaction
- */
-async function transferAsset(tx) {
-    tx.product.owner = tx.newOwner.personId;
-}
